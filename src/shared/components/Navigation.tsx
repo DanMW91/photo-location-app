30076730cc9e0d88ffe446d2bb61ddb7d2e417af
Daniel Maile-Wilson <danny.mw1991@gmail.com>
2022-01-25 14:49:33 +0000

    styling nav

--- src/shared/components/Navigation.tsx
+++ src/shared/components/Navigation.tsx
@@ -6,38 +6,49 @@ import RestoreIcon from '@mui/icons-material/Restore';
 import FavoriteIcon from '@mui/icons-material/Favorite';
 import LocationOnIcon from '@mui/icons-material/LocationOn';
 import { Link } from 'react-router-dom';
+import { Paper } from '@mui/material';
 
 const Navigation = (): JSX.Element => {
   const [value, setValue] = useState();
 
   return (
-    <Box sx={{ width: '100%', position: 'absolute', bottom: '0px' }}>
-      <BottomNavigation
-        showLabels
-        value={value}
-        onChange={(event, newValue) => {
-          setValue(newValue);
-        }}
-      >
-        <BottomNavigationAction
-          component={Link}
-          label="Recents"
-          to="/log"
-          icon={<RestoreIcon />}
-        />
-        <BottomNavigationAction
-          component={Link}
-          label="Favorites"
-          to="/favourites"
-          icon={<FavoriteIcon />}
-        />
-        <BottomNavigationAction
-          component={Link}
-          label="Nearby"
-          to="/map"
-          icon={<LocationOnIcon />}
-        />
-      </BottomNavigation>
+    <Box
+      sx={{
+        width: '100%',
+        position: 'absolute',
+        bottom: '0px',
+        '@media (min-width:780px)': { top: '0px' },
+      }}
+    >
+      <Paper>
+        <BottomNavigation
+          sx={{ backgroundColor: 'grey.400' }}
+          showLabels
+          value={value}
+          onChange={(event, newValue) => {
+            setValue(newValue);
+          }}
+        >
+          <BottomNavigationAction
+            component={Link}
+            label="Recents"
+            to="/log"
+            icon={<RestoreIcon />}
+          />
+          <BottomNavigationAction
+            component={Link}
+            label="Favorites"
+            to="/favourites"
+            icon={<FavoriteIcon />}
+          />
+          <BottomNavigationAction
+            component={Link}
+            label="Nearby"
+            to="/map"
+            icon={<LocationOnIcon />}
+          />
+        </BottomNavigation>
+      </Paper>
     </Box>
   );
 };
