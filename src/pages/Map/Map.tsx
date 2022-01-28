55e014219a1ef287f673330a0644ff21653b1144
Daniel Maile-Wilson <danny.mw1991@gmail.com>
2022-01-28 11:38:55 +0000

    remove mapbox, install gmaps

---
+++ src/pages/Map/Map.tsx
@@ -0,0 +1,10 @@
+import React, { useState, useEffect, useRef } from 'react';
+import MAP_API_KEY from '../../dev-api-key';
+import { Wrapper, Status } from '@googlemaps/react-wrapper';
+import Marker from './Marker';
+
+const Map = () => {
+  return <div></div>;
+};
+
+export default Map;
---
+++ src/pages/Map/Marker.js
@@ -0,0 +1,8 @@
+import mapboxgl from 'mapbox-gl';
+import './Marker.css';
+
+const Marker = () => {
+  return <div className="marker"></div>;
+};
+
+export default Marker;
